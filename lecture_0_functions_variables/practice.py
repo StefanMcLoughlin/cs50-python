e6c8486d5233bda5e413d25adcdef172c9cf4487
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-15 09:16:35 +0200

    Added F-Strings

--- lecture_0_functions_variables/practice.py
+++ lecture_0_functions_variables/practice.py
@@ -1,6 +1,11 @@
+# Frage nach dem Namen
 name = input("Wie heisst Du? ").strip().title()
-print("Willkommen,",name)
+print(f"Willkommen, {name}")
+
+# Frage nach dem Wohnort
 city = input("Wo wohnst Du? ").strip().capitalize()
-print("Du wohnst in", city)
-food = input("Was ist Dein Lieblingsessen? ").strip()
-print("Dein Lieblingsessen ist", food)
+print(f"Du wohnst in {city}")
+
+# Frage nach dem Lieblingsessen
+food = input("Was ist Dein Lieblingsessen? ").strip().capitalize()
+print(f"Dein Lieblingsessen ist {food}")
