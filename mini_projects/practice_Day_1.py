1a6d248ec9f708231a53f79f1c9e2e01324af084
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-16 09:20:19 +0200

    Einfache Tradinglogik erstellt

--- mini_projects/practice_Day_1.py
+++ mini_projects/practice_Day_1.py
@@ -1,10 +1,12 @@
-coin = input("Welcher Coin ist dein Favorit? ").lower()
+# Uebung 4 - Einfache Trading Logik
+# Fragestellung: Gewinn? Verlust? - Wenn Gewinn groesser als Verlust -> Strategie profitabel, sonst -> Strategie nicht profitabel
 
-if coin == "btc" or coin == "bitcoin":
-    print("Der Klassiker")
-elif coin == "sol" or coin == "solana":
-    print("Solana Ecosystem")
-elif coin == "doge" or coin == "dogecoin":
-    print("Meme Power")
+gewinn = int(input("Bitte trage die Summe der Gewinntrades ein: "))
+verlust = int(input("Bitte trage die Summe der Verlusttrades ein: "))
+
+if gewinn > verlust:
+    print("Strategie profitabel")
+elif gewinn == verlust:
+    print("Breakeven")
 else:
-    print("Interessante Wahl")
+    print("Strategie nicht profitabel")
