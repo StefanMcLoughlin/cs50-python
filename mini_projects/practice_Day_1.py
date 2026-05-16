5a6615b6004a2854ed30f3832f66144465dd772c
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-16 09:37:39 +0200

    Changed logic to keywords in text

--- mini_projects/practice_Day_1.py
+++ mini_projects/practice_Day_1.py
@@ -2,11 +2,11 @@
 # Fragestellung: Frage den Benutzer: Welches Problem hast Du? Wenn "API"-> "Pruefe JSON response", wenn "Python" -> "Ueberpruefe einrueckung", wenn "Login" -> "Pruefe Benutzernamen und Passwort", sonst -> "Unbekannter Fehler"
 
 problem = input("Welches Problem hast Du? ").lower()
-if problem == "api":
+if "api" in problem:
     print("Prüfe die JSON response")
-elif problem == "python":
+elif "python" in problem :
     print("Überprüfe die Einrückung")
-elif problem == "login":
+elif "login" in problem:
     print("Prüfe Benutzernamen und Passwort")
 else:
     print("Unbekannter Fehler")
