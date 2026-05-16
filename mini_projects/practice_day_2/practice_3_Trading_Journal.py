ecc8eeda86b240e67be733cfdae6837dc7585b8d
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-16 16:12:20 +0200

    Added new exercise - Aufgabe 3 - Tradingjournal

---
+++ mini_projects/practice_day_2/practice_3_Trading_Journal.py
@@ -0,0 +1,24 @@
+"""
+Übung 3 — Trading Journal Mini-Version
+Aufgabe:
+
+Frage:
+
+Anzahl Gewinntrades
+Anzahl Verlusttrades
+
+Berechne:
+
+Gesamtanzahl
+Gewinnquote in %
+Beispiel:
+Gewinntrades: 7
+Verlusttrades: 3
+
+Gewinnquote: 70%
+Bonus:
+
+Wenn Gewinnquote > 60:
+→ „Starke Strategie“
+"""
+
