ef7988cdc0533bb19429678cd226413bb4458a12
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-18 18:37:29 +0200

    Added categories for tests

--- lecture_5_unit_tests/practice_2_test_calculator.py
+++ lecture_5_unit_tests/practice_2_test_calculator.py
@@ -1,9 +1,15 @@
 from practice_1_calculator import square
 
-def test_square():
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+def test_zero():
     assert square(0) == 0
-    
+
+
+# Um die Funktion zu testen, muss "py -m pytest practice_2_test_calculator.py" im Terminal eingegeben werden.
