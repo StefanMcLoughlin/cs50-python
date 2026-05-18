34a1e322fef626ea87ea1f8f7dba318ceaf8fd34
Stefan McLoughlin <meckie1991@gmail.com>
2026-05-18 18:25:31 +0200

    Installed pytest and ran tests with pytest

--- lecture_5_unit_tests/practice_2_test_calculator.py
+++ lecture_5_unit_tests/practice_2_test_calculator.py
@@ -1,18 +1,9 @@
 from practice_1_calculator import square
 
-def main():
-    test_square()
-
-
 def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 zum Quadrat war nicht 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 zum Quadrat war nicht 9")
-
-if __name__ == "__main__":
-    main()
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
+    
